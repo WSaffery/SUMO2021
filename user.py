4d5ee1d57e07648a112602326680f33077e50e60
Kyle McLean <k.mclean@blueprintlab.com>
2021-09-28 21:13:33 +1000

    Change comments to reflect data correctly. Prevent UUV being under jaws.

--- user.py
+++ user.py
@@ -38,28 +38,30 @@ class User:
             The latest camera image frame.
 
         global_poses: Dict[str, np.ndarray]
-            A dictionary with the global camera and end effector positions. Keys are
-            'camera_end_joint' and 'end_effector_joint', values are vec3.
+            A dictionary with the global camera and end-effector pose. The keys are
+            'camera_end_joint' and 'end_effector_joint'. Each pose consitst of a (3x1)
+            position (vec3) and (4x1) quaternion defining the orientation.
         
         calcIK: function, (pos: np.ndarray, orient: np.ndarray = None) -> Dict[str, float]
-            Function to calculate inverse kinematics. Provide a desired end effector
+            Function to calculate inverse kinematics. Provide a desired end-effector
             position (vec3) and an orientation (quaternion) and it will return a pose
             dictionary of joint angles to approximate the pose.
         """
         
         cv2.imshow("View", image)
         cv2.waitKey(1)
-
+        
         # THIS IS AN EXAMPLE TO SHOW YOU HOW TO MOVE THE MANIPULATOR
-        if self.pose["bravo_axis_f"] > math.pi:
+        if self.pose["bravo_axis_e"] > math.pi:
             self.inc = -0.1
-        if self.pose["bravo_axis_f"] < math.pi * 0.5:
+        
+        if self.pose["bravo_axis_e"] < math.pi * 0.5:
             self.inc = 0.1
-        self.pose["bravo_axis_f"] += self.inc
+        
+        self.pose["bravo_axis_e"] += self.inc
 
         # EXAMPLE USAGE OF INVERSE KINEMATICS SOLVER
         #   Inputs: vec3 position, quaternion orientation
         # self.pose = calcIK(np.array([0.8, 0, 0.4]), np.array([1, 0, 0, 0]))
 
-        
         return self.pose
