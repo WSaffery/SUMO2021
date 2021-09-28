edcac0a6a3bf62850af9ba8d73e1815b744c35ab
John Sumskas <j.sumskas@blueprintlab.com>
2021-09-28 11:32:46 +1000

    Updated, using the latest automatically generated bravo_7_example.urdf from BPL_SDK

--- user.py
+++ user.py
@@ -6,13 +6,13 @@ import math
 class User:
     def __init__(self) -> None:
         self.pose = {
-            "bravo_7_axis_a": 0, 
-            "bravo_7_axis_b": 0, 
-            "bravo_7_axis_c": math.pi * 0.,
-            "bravo_7_axis_d": math.pi, 
-            "bravo_7_axis_e": math.pi * 0.0, 
-            "bravo_7_axis_f": math.pi * 0.5, 
-            "bravo_7_axis_g": math.pi
+            "bravo_axis_a": 0,
+            "bravo_axis_b": 0,
+            "bravo_axis_c": math.pi * 0.,
+            "bravo_axis_d": math.pi,
+            "bravo_axis_e": math.pi * 0.0,
+            "bravo_axis_f": math.pi * 0.5,
+            "bravo_axis_g": math.pi
         }
         self.inc = 0.1
         return
@@ -20,9 +20,9 @@ class User:
     def run(self, image: list, new_pose: Dict[str, float]) -> Dict[str, float]:
 
         # THIS IS AN EXAMPLE TO SHOW YOU HOW TO MOVE THE MANIPULATOR
-        if self.pose["bravo_7_axis_f"] > math.pi:
+        if self.pose["bravo_axis_f"] > math.pi:
             self.inc = -0.1
-        if self.pose["bravo_7_axis_f"] < math.pi * 0.5:
+        if self.pose["bravo_axis_f"] < math.pi * 0.5:
             self.inc = 0.1
-        self.pose["bravo_7_axis_f"] += self.inc
+        self.pose["bravo_axis_f"] += self.inc
         return self.pose
