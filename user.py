5f1bff7db9441aa771888341a05e478e63cbb0c8
WSaffery <William.Saffery@gmail.com>
2021-09-29 13:09:02 +1000

    added manual control for debug

--- user.py
+++ user.py
@@ -18,14 +18,37 @@ class User:
             "bravo_axis_f": math.pi * 0.9,
             "bravo_axis_g": math.pi
         }
+        self.oldpose = self.pose
         self.inc = 0.1
         self.last_time = time.time()
 
         return
 
+    def manual_control(self, global_poses, calcIK):
+        lineIn = input("input command: ")
+        commandList = lineIn.split(" ")
+        command = commandList[0]
+        args = commandList[1:]
+        if command == "data":
+            print(f"{global_poses['camera_end_joint']=}")
+            print(f"{global_poses['end_effector_joint']=}")
+            print(f"{self.pose=}")
+            print(f"{self.inc=}")
+        if command == "p":
+            print(np.array(args[0:3]), np.array(args[3:7]))
+            vec3 = np.array([float(x) for x in args[0:3]])
+            ori = np.array([float(x) for x in args[3:7]])
+            print(vec3, ori)
+            self.pose = calcIK(vec3, ori)
+        elif command == "inc":
+            self.inc = args[0]
+        elif command == "camera":
+            global_poses['camera_end_joint'] = args[0]
+        elif command == "end":
+            global_poses['end_effector_joint'] = args[0]
 
     def run(self,
-            image: list, 
+            image: list,
             global_poses: Dict[str, np.ndarray],
             calcIK: Callable[[np.ndarray, Optional[np.ndarray]], Dict[str, float]],
             ) -> Dict[str, float]:
@@ -40,24 +63,31 @@ class User:
             A dictionary with the global camera and end-effector pose. The keys are
             'camera_end_joint' and 'end_effector_joint'. Each pose consitst of a (3x1)
             position (vec3) and (4x1) quaternion defining the orientation.
-        
+
         calcIK: function, (pos: np.ndarray, orient: np.ndarray = None) -> Dict[str, float]
             Function to calculate inverse kinematics. Provide a desired end-effector
             position (vec3) and an orientation (quaternion) and it will return a pose
             dictionary of joint angles to approximate the pose.
         """
-        
+
         cv2.imshow("View", image)
         cv2.waitKey(1)
-        
+        if self.oldpose == self.pose:
+            try:
+                self.manual_control(global_poses, calcIK)
+            except Exception as e:
+                print(e)
+        else:
+            self.oldpose = self.pose
+
         # THIS IS AN EXAMPLE TO SHOW YOU HOW TO MOVE THE MANIPULATOR
-        if self.pose["bravo_axis_e"] > math.pi:
-            self.inc = -0.1
-        
-        if self.pose["bravo_axis_e"] < math.pi * 0.5:
-            self.inc = 0.1
-        
-        self.pose["bravo_axis_e"] += self.inc
+        # if self.pose["bravo_axis_e"] > math.pi:
+        #     self.inc = -0.1
+        #
+        # if self.pose["bravo_axis_e"] < math.pi * 0.5:
+        #     self.inc = 0.1
+        #
+        # self.pose["bravo_axis_e"] += self.inc
 
         # EXAMPLE USAGE OF INVERSE KINEMATICS SOLVER
         #   Inputs: vec3 position, quaternion orientation
